d4b79f8f3ac5a260329ee6f8e96b4939528bcb65
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-24 06:53:48 +0100

    Initial commit of empty plugin

---
+++ zsh-tig-plugin.plugin.zsh
@@ -0,0 +1,12 @@
+# According to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/$0}
+
+# Then ${0:h} to get plugin's directory
+
+if [[ ${zsh_loaded_plugins[-1]} != */zsh-tig-plugin && -z ${fpath[(r)${0:h}]} ]]
+then
+    fpath+=( "${0:h}" )
+fi
