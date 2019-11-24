35a18b9b721fb4590ea03cec9f419e59e450e8e2
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-24 12:54:34 +0100

    plugin.zsh: Add comments

--- zsh-tig-plugin.plugin.zsh
+++ zsh-tig-plugin.plugin.zsh
@@ -4,15 +4,19 @@
 0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
 0=${${(M)0:#/*}:-$PWD/$0}
 
-# Then ${0:h} to get plugin's directory
-
 if [[ ${zsh_loaded_plugins[-1]} != */zsh-tig-plugin && -z ${fpath[(r)${0:h}]} ]]
 then
     fpath+=( "${0:h}" )
 fi
 
+# END OF THE STANDARD BLOCK
+
+# Export the parameter just in case zsh_tig_plugin_update
+# will be run as script and something goes wrong with its
+# detection of the plugin dir
 typeset -gx ZSH_TIG_DIR=${0:h}
 
+# The alias that points tig to the extended tigrc
 alias tig='TIGRC_USER=$ZSH_TIG_DIR/tigrc tig'
 
 autoload zsh_tig_plugin_update
@@ -20,4 +24,5 @@ autoload zsh_tig_plugin_update
 (( ${+functions[@zsh-plugin-run-on-update]} )) && \
     @zsh-plugin-run-on-update "zsh_tig_plugin_update"
 
+# Generate initial tigrc
 [[ ! -f $ZSH_TIG_DIR/tigrc ]] && zsh_tig_plugin_update
